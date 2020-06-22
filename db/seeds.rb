596c3b09d56c05a67f59ace1b7e73a2ef0f9c24a
FelipeEnne <felipeenne@gmail.com>
2020-06-22 11:13:38 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(
+    name: 'Felipe',
+    email: 'felipe@gmail.com',
+    password_digest: '123',
+    favorite: '',
+)
